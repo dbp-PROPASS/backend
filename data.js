393f3cd170cef43e459eb3fa1a225f48cf33441f
Syeong <pyqw_@naver.com>
2024-11-29 18:23:06 +0900

    chore: remove unused dummy data

--- data.js
+++
@@ -1,8 +0,0 @@
-// 더미데이터
-
-const users = [
-    { id: 1, email: "test@example.com", password: "1234" },
-    { id: 2, email: "user@example.com", password: "abcd" }
-  ];
-  
-  module.exports = users;
