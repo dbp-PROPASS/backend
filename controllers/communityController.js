631a39c12ddf67effd6a2ac9b3c8b00c408ba96d
kyungeune <kyungeunhehe@gmail.com>
2024-11-30 12:21:23 +0900

    분야, 닉네임 뷰 추가

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -1,3 +1,4 @@
+// communityController.js
 const oracledb = require('oracledb');
 const {getConnection} = require('../config/dbConfig'); // DB 설정 파일 경로
 
@@ -8,6 +9,13 @@ const community = async (req, res) => {
     it: '2',
     english: '3',
     finance: '4',
+    tech: '5',
+    medical: '6',
+    edu: '7',
+    design: '8',
+    food: '9',
+    architect: '10',
+    national: '11',
   };
   const comId = String(categoryToComId[category]); // 명시적으로 문자열 변환  
 
@@ -21,11 +29,12 @@ const community = async (req, res) => {
     connection = await getConnection();
 
     const query = `
-      SELECT p.POST_ID, p.POST_TITLE, p.POST_CONTENT, p.MEM_ID
+      SELECT p.POST_ID, p.POST_TITLE, p.POST_CONTENT, u.NICKNAME
       FROM COMMUNITYPOST p
       JOIN COMMUNITY c ON p.COM_ID = c.COM_ID
+      JOIN MEMBER u ON p.MEM_ID = u.MEM_ID
       WHERE TRIM(p.COM_ID) = :comId
-      ORDER BY p.POST_ID DESC
+      ORDER BY p.POST_TITLE DESC
     `;
     const result = await connection.execute(query, { comId: String(comId) });
 
@@ -34,7 +43,7 @@ const community = async (req, res) => {
         postId: row[0],
         title: row[1],
         content: row[2],
-        author: row[3], // MEM_ID를 author로 매핑
+        author: row[3], // NICKNAME을 author로 설정
       }))
     );
   } catch (error) {
